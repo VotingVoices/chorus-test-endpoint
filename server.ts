0d47920d072a24d1a087a97771e97a81edc11212
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2018-10-05 22:17:40 -0700

    Use body-parser

--- server.ts
+++ server.ts
@@ -1,14 +1,19 @@
 import express = require("express");
+import * as bodyParser from 'body-parser';
 
 const router = express.Router();
 
 router.post('/', (req: express.Request, res: express.Response) => {
 	console.log(`${req.method}: ${req.body}`);
+	console.log(req.body);
 	res.status(200).json({ result: 'success' });
 });
 
 const app = express();
 const port = 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(router);
 
